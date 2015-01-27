f76991969869342c2a7209a5201381126800f5de
范杨 <fanyang.coder@gmail.com>
2015-01-28 03:41:11 +0800

    imp rpc resquest

---
+++ rpc_request/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    zmq "github.com/alecthomas/gozmq"
+    //sj "github.com/bitly/go-simplejson"
+    "time"
+)
+
+func main() {
+    context, _ := zmq.NewContext()
+    defer context.Close()
+
+    router, _ := context.NewSocket(zmq.DEALER)
+    defer router.Close()
+
+    router.SetIdentity("test-server-1")
+    router.Connect("tcp://localhost:3333")
+    id := 1
+
+    for {
+        time.Sleep(1 * time.Second)
+        data_format := `
+{
+  "id": %d,
+  "msg": {
+    "namespace": "user",
+    "serverType": "test",
+    "service": "service",
+    "method": "echo",
+    "args": [
+      "%s"
+    ]
+  }
+}
+`
+        msg := fmt.Sprintf("hello %d", id)
+        data := fmt.Sprintf(data_format, id, msg)
+        id++
+        router.Send([]byte(data), 0)
+
+        parts, err := router.RecvMultipart(0)
+
+        if err != nil {
+            fmt.Printf("err %s\n", err.Error())
+            continue
+        }
+        fmt.Printf("data  %s\n", parts)
+        fmt.Printf("data 0  %s\n", parts[0])
+    }
+}
